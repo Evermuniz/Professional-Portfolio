f17c77807ef04ebeafb39d68eb5050207cf4b017
Evermuniz <evermuniz@outlook.com>
2023-07-16 16:07:25 -0600

    bootstrap and navbar

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavTabs from './Navbar';
+
+function Header() {
+    return (
+        <header>
+        <h1>Ever Muniz</h1>
+        <NavTabs />
+        </header>
+    );
+    }
+
+export default Header;

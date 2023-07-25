84acd24e209ff164ff2526430d53ad8ef4af7846
Evermuniz <evermuniz@outlook.com>
2023-07-25 11:22:40 -0600

    screenshots and finishing touches on portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,8 +3,12 @@ import "./../../styles/Section.css";
 import "./../../styles/Portfolio.css";
 import githubLogo from "./../../Images/github.svg";
 import launchLogo from "./../../Images/box-arrow-up-right.svg";
-import techBlogScreenshot from "./../../Images/techBlogScreenshot.jpeg";
-import testScreenshot from "./../../Images/screenshot.jpeg";
+import techBlog from "../../Images/techBlog.jpg"
+import become from "../../Images/become.jpg"
+import jate from "../../Images/jate.jpg"
+import JSQuiz from "../../Images/JSQuiz.jpg"
+import weatherDashboard from "../../Images/weatherDashboard.jpg"
+import noteTaker from "../../Images/noteTaker.jpg"
 
 export default function Portfolio() {
   const projects = [
@@ -12,48 +16,42 @@ export default function Portfolio() {
       name: "The Tech Blog",
       deployed: "https://muniz-tech-blog-62e3233fa5a7.herokuapp.com/",
       github: "https://github.com/Evermuniz/Tech-Blog",
-      image: testScreenshot,
-      technologies: "JavaScript, Node.js, Express.js, MySQL, Sequelize, Handlebars.js, Bcrypt, Dotenv, Heroku",
+      image: techBlog,
     },
     {
       name: "Become",
       deployed: "https://becomethebestyou.herokuapp.com/",
       github: "https://github.com/tylermifflin/Become",
-      image: testScreenshot,
-      technologies: "JavaScript, Node.js, Express.js, MySQL, Sequelize, Handlebars.js, Bcrypt, Dotenv, Heroku",
+      image: become,
     },
     {
       name: "Note Taker",
       deployed: "https://nameless-sea-71643.herokuapp.com/",
       github: "https://github.com/Evermuniz/Express.js-Note-Taker",
-      image: testScreenshot,
-      technologies: "JavaScript, Node.js, Express.js, Heroku",
+      image: noteTaker,
     },
     {
       name: "Weather Dashboard",
       deployed: "https://evermuniz.github.io/Weather-Dashboard/",
       github: "https://github.com/Evermuniz/Weather-Dashboard",
-      image: testScreenshot,
-      technologies: "JavaScript, HTML, CSS, Bootstrap, OpenWeather API",
+      image: weatherDashboard,
     },
     {
       name: "PWA Text Editor",
       deployed: "https://pwa-jate-2023-7c576433adaf.herokuapp.com/",
       github: "https://github.com/Evermuniz/PWA-Text-Editor",
-      image: testScreenshot,
-      technologies: "JavaScript, Node.js, Express.js, MongoDB, Mongoose, Heroku",
+      image: jate,
     },
     {
       name: "JavaScript Quiz",
       deployed: "https://evermuniz.github.io/JS-Fundamentals-Quiz/",
       github: "https://github.com/Evermuniz/JS-Fundamentals-Quiz",
-      image: testScreenshot,
-      technologies: "JavaScript, HTML, CSS",
+      image: JSQuiz,
     },
   ];
 
   const projectList = projects.map((project, index) => (
-    <div key={index} className="container">
+    <div key={index} className="container d-flex w-75">
       <div className="card col p-0 border border-5">
         <div>
           <h2 className="ps-2">{project.name}</h2>
@@ -61,7 +59,7 @@ export default function Portfolio() {
         </div>
 
         <div className="social row col-2">
-          <div className="d-flex align-items-center">
+          <div className="d-flex align-items-center pb-5">
             <i className="card1">
               <a
                 href={project.deployed}
